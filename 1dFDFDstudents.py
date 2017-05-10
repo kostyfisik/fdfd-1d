1590cd8fe59ce5d6b4dd9fdd5750cef30041c946
Konstantin Ladutenko <kostyfisik@gmail.com>
2017-05-10 13:38:54 +0300

    update task2

--- 1dFDFDstudents.py
+++ 1dFDFDstudents.py
@@ -1,48 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
 #%%
 # modes in PMC-PEC 1d cavity, analytical lambda=4a/(2*n+1)
 
-%matplotlib inline
-import numpy as np
-import scipy as sp 
-import scipy.sparse.linalg as ssl
-import matplotlib.pyplot as plt
+# #%matplotlib inline
+# import numpy as np
+# import scipy as sp 
+# import scipy.sparse.linalg as ssl
+# import matplotlib.pyplot as plt
 
-n=100 # number of grid nodes
-dx=1/n # discretization step, domain size = 1
+# n=100 # number of grid nodes
+# dx=1/n # discretization step, domain size = 1
 
-c = np.ones((1,n))/dx # column-vector to construct 1d differential operator
-F = sp.sparse.spdiags(np.vstack((-c, c)), np.array([0, 1]), n, n) # forward difference matrix
+# c = np.ones((1,n))/dx # column-vector to construct 1d differential operator
+# F = sp.sparse.spdiags(np.vstack((-c, c)), np.array([0, 1]), n, n) # forward difference matrix
 
-B = -sp.sparse.dia_matrix.transpose(F) # backward difference matrix
+# B = -sp.sparse.dia_matrix.transpose(F) # backward difference matrix
 
-q = -B*F # to assemble eigenmatrix
-# mp.pyplot.spy(sp.sparse.csr_matrix(q)) # to visualize matrix
-# mp.pyplot.show()
+# q = -B*F # to assemble eigenmatrix
+# # mp.pyplot.spy(sp.sparse.csr_matrix(q)) # to visualize matrix
+# # mp.pyplot.show()
 
-kt = 2*sp.pi*1 # wave vector target to find resonance wavelength
-k2, V = ssl.eigs(q, k=6, M=None, sigma=kt**2) 
+# kt = 2*sp.pi*1 # wave vector target to find resonance wavelength
+# k2, V = ssl.eigs(q, k=6, M=None, sigma=kt**2) 
 
-k = np.sort(np.sqrt(k2)); # resonance wave vector 
+# k = np.sort(np.sqrt(k2)); # resonance wave vector 
 
-lam = 2*sp.pi/np.real(k); # wavelength
-Q = np.real(k)/(2*np.imag(k)); # quality factor
+# lam = 2*sp.pi/np.real(k); # wavelength
+# Q = np.real(k)/(2*np.imag(k)); # quality factor
 
-plt.hold(True)
-plt.plot(np.arange(1,6.1,1), lam, '-')
-plt.plot(np.arange(1,6.1,1), 4/(2*np.arange(0,5.1,1)+1), '*')
+# plt.hold(True)
+# plt.plot(np.arange(1,6.1,1), lam, '-')
+# plt.plot(np.arange(1,6.1,1), 4/(2*np.arange(0,5.1,1)+1), '*')
 
-f, ax = plt.subplots(6,1, sharex=True, sharey=True)
-for nOfInd in range(6):
-    ax[nOfInd].plot(V[:,nOfInd],'-')
+# f, ax = plt.subplots(6,1, sharex=True, sharey=True)
+# for nOfInd in range(6):
+#     ax[nOfInd].plot(V[:,nOfInd],'-')
      
 #%%
 # Band Gap Diagramm
 
-%matplotlib inline
+#%matplotlib inline
 import numpy as np
 import scipy as sp 
 import scipy.sparse.linalg as ssl
 import matplotlib.pyplot as plt
+from numpy import linalg as LA
 
 n=100 # number of grid nodes
 dx=1/n # discretization step, domain size = 1
@@ -67,20 +71,28 @@ for ik in range(kk0.size):
 
     q = -epsinv*B*F # to assemble eigenmatrix
 
+    # print(q)
+    
     kt = k0/np.sqrt((eps1+eps2)/2); # target k=w/c
-    k2, V = ssl.eigs(q, k=6, M=None, sigma=kt**2)
+    k2, V = ssl.eigs(q, k=6, M=None, sigma=kt**2, OPpart='r')
+    #k2, V = LA.eig(q)
     
     k[:,ik] = np.sqrt(k2) # k=w/c
 
 lam = 2*sp.pi/np.real(k)
-Q = np.real(k)/(2*np.imag(k))
+#Q = np.real(k)/(2*np.imag(k))
     
 for nOfInd in range(6):
     plt.hold(True)
     plt.plot(kk0/(2*sp.pi), k[nOfInd,:]/(2*sp.pi) ,'-')
+plt.xlim([-0.5, 0.5])
+plt.ylim([0.0, 0.3])
+plt.savefig("fdfd-bg.png",pad_inches=0.02, bbox_inches='tight')
 
 f, ax = plt.subplots(6,1, sharex=True, sharey=True)
 
 for nOfInd in range(6):
     ax[nOfInd].plot(V[:,nOfInd],'-');
-              
+
+plt.savefig("fdfd-modes.png",pad_inches=0.02, bbox_inches='tight')
+
